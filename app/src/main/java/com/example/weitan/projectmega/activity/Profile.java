ea87d983e634124108db051816268afa1f23d0ae
machinegunjoeviterbo <57130500262@st.sit.kmutt.ac.th>
2017-09-09 18:35:42 +0700

    Added radio buttons for Gender and date picker for Birthday

--- app/src/main/java/com/example/weitan/projectmega/activity/Profile.java
+++ app/src/main/java/com/example/weitan/projectmega/activity/Profile.java
@@ -1,15 +1,62 @@
 package com.example.weitan.projectmega.activity;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import com.example.weitan.projectmega.R;
 
+import java.util.Calendar;
+
 public class Profile extends AppCompatActivity {
+
+    private static final String TAG = "Profile";
+
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        mDisplayDate = (TextView) findViewById(R.id.tv_birthday);
+
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        Profile.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                Log.d(TAG, "onDateSet: mm/dd/yyyy: " + month + "/" + day + "/" + year);
+
+                String date = day + "-" + month + "-" + year;
+                mDisplayDate.setText(date);
+            }
+        };
+
     }
+
 }
