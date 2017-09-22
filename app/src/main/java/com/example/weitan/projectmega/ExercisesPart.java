b8ecaef5a3cfd5664102a3400374599964b37331
csievan <wuweitan5@gmail.com>
2017-09-23 02:04:59 +0700

    fixed login, sign out, and switch to another count problem and added swipeview to the exercise layout

--- app/src/main/java/com/example/weitan/projectmega/ExercisesPart.java
+++ app/src/main/java/com/example/weitan/projectmega/ExercisesPart.java
@@ -107,8 +107,6 @@ public class ExercisesPart extends AppCompatActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_exercises_one, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
         }
     }
