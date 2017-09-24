f4da04d0ba46c27022ac6b4391d57b7031ea66f6
machinegunjoeviterbo <57130500262@st.sit.kmutt.ac.th>
2017-09-24 21:48:26 +0700

    Fixed the layout of Exercises activity

---
+++ app/src/main/java/com/example/weitan/projectmega/activity/Notifications.java
@@ -0,0 +1,15 @@
+package com.example.weitan.projectmega.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.weitan.projectmega.R;
+
+public class Notifications extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifications);
+    }
+}
