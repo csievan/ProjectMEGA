3621376f285114594079704c903ffda88ac4129f
machinegunjoeviterbo <57130500262@st.sit.kmutt.ac.th>
2017-09-24 20:26:18 +0700

    Fixed the Firebase Cloud Messaging to work properly

---
+++ app/src/main/java/com/example/weitan/projectmega/activity/FirebaseIDService.java
@@ -0,0 +1,36 @@
+package com.example.weitan.projectmega.activity;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by m on 2017-09-24.
+ */
+
+public class FirebaseIDService extends FirebaseInstanceIdService {
+    private static final String TAG = "FirebaseIDService";
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // Implement this method to send any registration to your app's servers.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    /**
+     * Persist token to third-party servers.
+     *
+     * Modify this method to associate the user's FCM InstanceID token with any server-side account
+     * maintained by your application.
+     *
+     * @param token The new token.
+     */
+    private void sendRegistrationToServer(String token) {
+        // Add custom implementation, as needed.
+    }
+}
